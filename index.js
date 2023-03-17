954521ea5e3e0d690496576130ff714a6f648e4b
Gustavo Costa <gustavo.lcostaa@gmail.com>
2023-03-17 07:08:42 -0700

    created the data,routes and images folders and the index file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+
+const userRoutes = require("./routes/users");
+
+app.use("/", userRoutes);
+app.use("/images", express.static("./public/images"));
+
+app.listen(PORT, function () {
+  console.log("BEGIN server.");
+});
